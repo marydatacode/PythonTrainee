4d029cb3ef756f473372321321a345b28e9f11e3
Eduarda Marques <150185136+marydatacode@users.noreply.github.com>
2023-11-20 21:38:29 -0300

    Interators e Loops

--- Interators e Loops em Python.py
+++ Interators e Loops em Python.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
 
 word = "1234567987654321234567876543212345"
 nums = ['0','2','4','6','8']
@@ -15,55 +10,27 @@ for character in word:
         
 print(numsCount)
 
-
-# In[4]:
-
-
 mytuple = ('gol','saveiro','fusca')
 
 for x in mytuple:
     print(x)
 
-
-# In[5]:
-
-
 for x in range(9):
     print(x)
 
-
-# In[8]:
-
-
 for x in range(0,22,2):
     print(x)
-
-
-# In[11]:
-
 
 pares = [0,2,4,6,8]
 for x in pares:
         print(x)
 
-
-# In[12]:
-
-
 frutas = ['uva','banana','limão']
 for x in frutas:
     print(x)
 
-
-# In[20]:
-
-
 for x in 'python':
     print(x)
-
-
-# In[22]:
-
 
 paises = ['alemanha','australia','estados unidos','japao']
 for x in paises:
@@ -71,38 +38,22 @@ for x in paises:
     if x == 'estados unidos':
         break
 
-
-# In[25]:
-
-
 linguagens = ['cobol','pythoon','c++']
 for x in linguagens:
     if x == 'python':
         continue
     print(x)
 
-
-# In[27]:
-
-
 for x in range(7):
     print(x)
 else:
     print('concluido')
-
-
-# In[28]:
-
 
 for x in range(10):
     if x == 5: break
     print(x)
 else:
     print('concluido')
-
-
-# In[29]:
-
 
 # Nested Loops
 
@@ -112,10 +63,5 @@ carros = ['tiguan','audi','mercedes']
 for x in cores: 
     for y in carros:
         print(x,y)
-
-
-# In[ ]:
-
-
 
 
